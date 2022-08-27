b2afabcfaa6bad354f4b7085ecbc42b7b3e3bf09
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-08-27 09:37:25 -0400

    add thought routes to index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
+// adding prefix users and thoughts to the routes created in their respective files
 router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
